55ef436a1d444db4f0c81cc2da8f4319acbe88d3
Wang Ruochen <wangkevin920@163.com>
2020-03-11 13:29:44 +0800

    Add checks for invalid date

--- src/util.rs
+++ src/util.rs
@@ -1,21 +1,9 @@
 use crate::errors::Errors;
+use chrono::naive::NaiveDate;
+use chrono::Datelike;
 
-pub fn parse_date(date: &str) -> Result<(u32, u32, u32), Errors> {
-    let numbers: Vec<u32> = date
-        .split('-')
-        .map(|s| Ok(s.parse()?))
-        .collect::<Result<_, _>>()
-        .map_err(|_: std::num::ParseIntError| Errors::InvalidDate)?;
-    match *numbers.as_slice() {
-        [y, m, d] => Ok((y, m, d)),
-        _ => Err(Errors::InvalidDate),
-    }
-}
-
-#[cfg(tests)]
-mod tests {
-    #[test]
-    fn date() {
-        //
-    }
+pub fn parse_date(date: &str) -> Result<(i32, u32, u32), Errors> {
+    date.parse::<NaiveDate>()
+        .map(|d| (d.year(), d.month(), d.day()))
+        .map_err(|_| Errors::InvalidDate)
 }
